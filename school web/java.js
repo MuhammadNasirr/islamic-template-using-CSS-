0775c58c15571a2d2515120995ba059b7ee07264
salman <salmanmasood24@yahoo.com>
2016-07-09 16:14:47 +0500

    school temp added

---
+++ school web/java.js
@@ -0,0 +1,96 @@
+
+A=new Array("images/s1.jpg","images/s2.jpg","images/s3.jpg","images/s4.jpg","images/s5.jpg","images/bg.jpg");
+function c1()
+{
+ document.getElementById("sliderimg").src = A[0];
+ 
+}
+function c2()
+{
+ document.getElementById("sliderimg").src = A[1];
+ 
+}
+function c3()
+{
+ document.getElementById("sliderimg").src = A[2];
+ 
+}
+function c4()
+{
+ document.getElementById("sliderimg").src = A[3];
+ 
+}
+function c5()
+{
+ document.getElementById("sliderimg").src = A[4];
+ 
+}
+
+function myfn()
+{
+
+setTimeout(c1,10000);
+setTimeout(c2,20000);
+setTimeout(c3,30000);
+setTimeout(c4,40000);
+setTimeout(c5,50000);
+setTimeout(c1,60000);
+setTimeout(c2,70000);
+	
+	
+}
+function aslam()
+{
+	
+document.getElementById('idimg').src="images/tb.jpg";
+document.getElementById('name').innerHTML="Aslam SaGar";
+document.getElementById('edu').innerHTML="MA-Mathematics";
+document.getElementById('exp').innerHTML="25 Years experince of Teahcing Mathematics & physics";
+document.getElementById('des').innerHTML="Physics Teahcer";
+document.getElementById('email').innerHTML="AslamSaqar@yahoo.com";
+	
+	
+}
+
+function salman()
+{
+	
+document.getElementById('idimg').src="images/salman.jpg";
+document.getElementById('name').innerHTML="Salman Masood";
+document.getElementById('edu').innerHTML="Masters Of Computer Science & BSC Mathematics";
+document.getElementById('exp').innerHTML="4 years Teahcing Experience of Mathematics";
+document.getElementById('des').innerHTML="Mathematics Teacher ";
+document.getElementById('email').innerHTML="salmanmasood24@yahoo.com";
+		
+}
+function rizwan()
+{
+	
+document.getElementById('idimg').src="images/tc.jpg";
+document.getElementById('name').innerHTML="Rizwan Ahmed";
+document.getElementById('edu').innerHTML="MBA finance";
+document.getElementById('exp').innerHTML="4 years Teahcing Experience of Computer & English";
+document.getElementById('des').innerHTML="Computer Teacher & Trainer ";
+document.getElementById('email').innerHTML="Rizwanlycos@yahoo.com";
+		
+}
+function zunair()
+{
+	
+document.getElementById('idimg').src="images/td.jpg";
+document.getElementById('name').innerHTML="Zunair Ali";
+document.getElementById('edu').innerHTML="MBA Marketing";
+document.getElementById('exp').innerHTML="4 years Teahcing Experience of English";
+document.getElementById('des').innerHTML="English Teacher  ";
+document.getElementById('email').innerHTML="ZunairALIlycos@yahoo.com";
+		
+}
+
+
+
+
+
+
+
+
+
